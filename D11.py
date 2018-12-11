59694b6283a810ab4307c44493e66bb92fa82a15
toastedyeti <VHSbuddha@github.com>
2018-12-11 21:13:16 +0100

    P1 /  P2 Incomplete

--- D11.py
+++ D11.py
@@ -1,4 +1,12 @@
 import time
+import numpy as np
+
+'''
+Stuff Learned:
+    - I would not have thought to subtract the size from the range in P2
+    - np.sum & grid[] !!!!!
+'''
+
 
 def hundreds(numbers):
     one = numbers%10
@@ -38,9 +46,13 @@ def cellValue(x,y,c,s):
         for y in yr:
             r.append((x,y))
     for i in r:
-        if i[0] + s < 300 and i[1] + y  < 300:
             if i in c:
-                outsum += int(c[i])
+                # this may produce incorrect for P1, as it was altered (incorrectly) for part2
+                # attempting to set a boundary to skip if the grid looks outside of
+                if i[0] + s <= 300 and i[1] + s  <= 300:
+                    outsum += int(c[i])
+                else:
+                    pass
     return outsum
 
 def part1(serialNum, size, s):
@@ -55,18 +67,41 @@ def part1(serialNum, size, s):
             coordCapture = [(i[0], i[1])]
     return maxPower, coordCapture
 
-def part2(serialNum):
+def power(X,Y,S):
+    # u/jonathan_paulson
+    rack_id = X+10
+    power = rack_id * Y + S
+    power = power * rack_id
+    power = (power/100)%10
+    power -= 5
+    return power
+
+def part2(serialNum): #-- Does not give the right solution
     maxPower = 0
     gridSize = 0
     coordCapture = []
-    for i in range(1,301):
-        print(i, maxPower, gridSize, coordCapture)
-        mp, cc = part1(serialNum, 300, i)
-        if int(mp) > maxPower:
-            maxPower = int(mp)
-            gridSize = i
-            coordCapture = [(cc)]
-    return maxPower, coordCapture, gridSize
+    # Numpy ... not mine... using to learn
+    grid = np.fromfunction(lambda x, y :power(x, y, serialNum), (300, 300))
+    for size in range(3,300): # 3-300 starts from part1() grid size
+        if not size % 10: # nice trick to print 10 count tracking
+            print(size)
+        for x in range(300-size): 
+            for y in range(300-size):
+                if y>size-1 and x>size-1: #again u/jonathan_paulson 
+                    mp = np.sum(grid[x:x+size, y:y+size])
+                    if mp > maxPower:
+                        maxPower = mp
+                        coordCapture = [x,y]
+                        gridSize = size
+    return  coordCapture, gridSize
+
+        # Inefficient -- would take over 1-2 hours to run
+        # for i in range(1,301):
+            # mp, cc = part1(serialNum, 300, i)
+            # if int(mp) > maxPower:
+            #     maxPower = int(mp)
+            #     gridSize = i
+            #     coordCapture = [(cc)]
 
 #print(part1(3463, 300, 3))
 print(part2(3463))
