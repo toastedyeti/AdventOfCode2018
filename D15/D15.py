4193795e3a0b84aab6896345699aaa25799c8627
toastedyeti <VHSbuddha@github.com>
2018-12-16 17:19:37 +0100

    Incomplete

--- D15/D15.py
+++ D15/D15.py
@@ -3,53 +3,69 @@ import sys
 import collections
 
 class combatant:
-    def __init__(self, x, y):
+    def __init__(self, x, y, id):
         self.x = x
         self.y = y
-        self.hp = 
-    
+        self.id = id
+        self.hp = 200
+
+    def getid(self):
+        return self.id
+
     def beacon(self):
         return (self.x, self.y)
 
-    def recon(self):
+    def alive(self):
+        return self.hp > 0
+
+    def recon(self, enemies):
         cx = self.x
         cy = self.y
         zones = [(cx, cy+1), (cx+1, cy), (cx, cy-1), (cx-1,cy)]
-
+        for i in zones:
+            targets = [i for i in enemies if i.type != self.type and i.alive() == True]
+        return targets
+    
+    
 class goblin(combatant):
-    def __init__(self,x,y):
-       combatant.__init__(self,x,y)
+    type = "G"
 
 class elf(combatant):
-    def __init__(self,x,y):
-           combatant.__init__(self,x,y)
+    type = "E"
 
 class cave(object):
-
     positions = {}
+    elves = []
+    goblins = []
 
-    def update(self):
-        
 
 def setup(fname):
     with open(os.path.join(sys.path[0], fname), "r") as f:
         d15 = [l.strip() for l in f]
     battlefield = cave()
+    uid = 0
     for y,line in enumerate(d15):
         for x, piece in enumerate(line):
             if piece == "#":
                 battlefield.positions[(x,y)] = '#'
             elif piece == "G":
-                g = goblin(x,y)
-                battlefield.positions[(x,y)] = g
+                g = goblin(x,y, uid)
+                battlefield.positions[(x,y)] = '.'
+                battlefield.goblins.append(g)
+                uid += 1
             elif piece == "E":
-                e = elf(x,y)
-                battlefield.positions[(x,y)] = e
+                e = elf(x,y,uid)
+                battlefield.positions[(x,y)] = '.'
+                battlefield.elves.append(e)
+                uid += 1
     return battlefield
 
 def part1():
     battlefield = setup('d15.txt')
-    for i in range(0,20):
-        battlefield.update()
+    for i in battlefield.elves:
+        print(i.id, i.recon(battlefield.goblins),"\n")
+    # print(battlefield.positions)
+    # print(battlefield.elves)
+    # print(battlefield.goblins)
 
 part1()
