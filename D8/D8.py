b20d3e4b1e60e8c031049dba3e43b55302e46449
toastedyeti <VHSbuddha@github.com>
2018-12-20 21:24:20 +0100

    P1/P2 Complete
    Good lesson in recursion

--- D8/D8.py
+++ D8/D8.py
@@ -1,28 +1,62 @@
+'''
+Stuff Learned:
+- So many of the other AOC challenges needed a recursive solution, and my knowledge is shaky... thanks to u/sciyoshi for the clear solution
+- Recursion requires determining the trivial case, and if that cannot be implemented, passing a smaller portion of the problem down the line
+- Here the trivial case is the (A) no children and sum metadata for no children.  The function returns total and the remaining unparsed data
 
+Next:
+More recursion exercises from "thinking like a programmer"
+'''
 
-def openInput(fileName):
-    out = ''
-    with open(fileName) as f:
-        out = f.read()
-    out = [int(x) for x in out.split()]
+import sys, os
+
+dt = [2,3,0,3,10,11,12,1,1,0,1,99,2,1,1,2]
+
+def openInput(fname):
+    out = []
+    for i in open(os.path.join(sys.path[0], fname)).read().strip('\n').split(" "):
+        out.append(int(i))
     return out
 
-def part1():
-    pI = openInput('d8.txt')
-    # NODE
-        # Header -- Always two numbers 
-            # quant child nodes
-            # quant meta entries
-        # Zero or more Child nodes
-        # One or more meta entries
-    # If the node has children, recursively parse the node data
-    
-    '''
-    2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+data = openInput('d8.txt')
 
-    <2,3> - [1,1,2]
-    /    \
-<0,3>   <1,1> -[99]
-  |         \
-[10,11,12]   <0,1> - [2]
-    '''
+def part1(data):
+    c, m = data[:2]
+    data = data[2:]
+    total = 0
+
+    for i in range(c):
+        t, data = part1(data)
+        total += t
+    
+    total += sum(data[:m])
+
+    if c == 0:
+        return (total, data[m:])
+    else: 
+        return (total, data[m:])
+
+t, data = part1(dt)
+print(t)
+
+def part2(data):
+    c, m = data[:2]
+    data = data[2:]
+    total = 0
+    scores = []
+
+    for i in range(c):
+        t, data,v = part2(data)
+        total += t
+        scores.append(v)
+    
+    total += sum(data[:m])
+
+    if c == 0:
+        return (total, data[m:], sum(data[:m]))
+    else:
+        v = sum(scores[k-1] for k in data[:m] if k > 0 and k<= len(scores))
+        return (total, data[m:], v)
+
+t, data, v = part2(dt)
+print(t,v)
