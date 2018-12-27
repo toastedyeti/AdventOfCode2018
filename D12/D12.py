ed83624aa73727e57e07b6481c69048806c7c9f4
toastedyeti <VHSbuddha@github.com>
2018-12-27 09:15:44 +0100

    Part 1/2 complete
    I might want to revisit this to learn how to implement stable finding solutions.  Repeating solution detection is useful for AoC

--- D12/D12.py
+++ D12/D12.py
@@ -3,65 +3,71 @@
 Stuff Learned:
 - eumerate() -- allows us to loop over something and have an automatic counter
 - parse library.... that was so much easier than spending 45 minutes getting the re expression just right
+- Running Part1 for 50B iterations would have taken an extremely long time... Other solutions on reddit look for a stable solution, 
+meaning 2 or more in diffs are the same indicating some predictable pattern.  This solution runs to 1000, averages the previous 100 diffs
+and adds the current plant sum to the total answer.  
+    * for x in range(generations):
+        pots = "....pots...."
+        diff = pots in pots
+        if diff in diffs then stable
 
 Remaining Questions:
-- Why is was adding "..." to each side of the current generation?  The total curr length increases by four each generation and printing
-each curr makes the # seem to move across the line and return to the begining 
+- Why was it necessary to add "...." to front/back of each generation?  Is there a way to manage the movement
+of the pots without adding additional characters?
+
 '''
 import parse as pr
+from collections import defaultdict
+import time
+import os, sys
 
-init = "#.#.#...#..##..###.##.#...#.##.#....#..#.#....##.#.##...###.#...#######.....##.###.####.#....#.#..##"
+def setup():
+    init = "#.#.#...#..##..###.##.#...#.##.#....#..#.#....##.#.##...###.#...#######.....##.###.####.#....#.#..##"
+    pattern = pr.compile("{} => {}")
+    rules = {}
+    with open(os.path.join(sys.path[0], "d12.txt"), "r") as f:
+        ins = [l.strip() for l in f.readlines()]
+    f.close()
 
-rules = {}
-pattern = pr.compile("{} => {}")
-ins = []
+    for i in ins:
+        rules[pattern.parse(i)[0]] = pattern.parse(i)[1]
 
-with open ('d12.txt') as f:
-    ins = [l.strip() for l in f.readlines()]
+    return rules, init
 
-for i in ins:
-    rule = pattern.parse(i)
-    rules[rule[0]] = rule[1]
-
-def plantcounter(status): 
-    diff = (len(status)-100)//2
-    ctr = 0
-    for i, c in enumerate(status):
+def plantc(curr):
+    diff = (len(curr) - 100) // 2
+    sum = 0
+    for i, c in enumerate(curr):
         if c == '#':
-            ctr += (i - diff)
-    return ctr
-    
+            sum += (i - diff)
+    return sum
 
 def part1():
-    curr = init
-    prev_sum = plantcounter(init)
-    diffs = []
-    iters = 21
+    rules, init = setup()
+    current = init
+    diffs  = []
+    previous_sum = plantc(init)
+    iters = 1000
     for i in range(iters):
-        if (i == 20):
-            print("Part 1: ", str(plantcounter(curr)))
-        curr = "...." + curr + "....."
-        #print(curr)         
-        #print(len(curr))
-        next = ""
-        for x in range(2, len(curr) - 2):
-            sub = curr[x-2:x+3]
-            next += rules[sub]
-        curr = next
-        currsum = plantcounter(curr)
-        diff = currsum - prev_sum
+        if i == 20:
+            print("PART 1: ", str(plantc(current)))
+            print(time.time() - t1)
+        current = "...." + current + "...."
+        next_ = ''
+        for x in range(2, len(current)-2):
+            sub = current[x-2:x+3]
+            next_ += rules[sub]
+        current = next_
+        current_sum = plantc(current)
+        diff = current_sum - previous_sum
         diffs.append(diff)
-        if(len(diffs) > 100): 
+        if (len(diffs) > 100):
             diffs.pop(0)
-            prev_sum = currsum
-
+        previous_sum = current_sum
     last100diff = sum(diffs) // len(diffs)
+    total = (50000000000 - iters) * last100diff + plantc(current)
+    print("PART 2: ", str(total))
 
-    total = (50000000000 - iters) * last100diff + plantcounter(curr) 
-
-    print("Part 2: " + str(total))
-
+t1 = time.time()
 part1()
-
-
 
